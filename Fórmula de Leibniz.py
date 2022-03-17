423e3396390a42e2f09f764d8383d8993ccae32d
GuilhermeMastelini <96960157+GuilhermeMastelini@users.noreply.github.com>
2022-03-17 17:14:45 -0300

    Delete Fórmula de Leibniz.py

--- Fórmula de Leibniz.py
+++
@@ -1,9 +0,0 @@
-# Cálculo de pi pelo método de Leibniz
-a = int(input('Método de Leibniz. Digite o núro de interações para melhorar a precisão '))
-b = 0
-
-for i in range(1, a + 1):
-    x = ((-1)**(i-1))*((4)/(2*i - 1))
-    b = b + x
-
-print (b)
